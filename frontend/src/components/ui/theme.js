bde645ef2ff29ec50cac2969e4d893e01758df9c
samroodAli <devsamrood@gmail.com>
2021-10-21 00:41:31 +0530

    Typography:Create styles for headings and body

--- frontend/src/components/ui/theme.js
+++ frontend/src/components/ui/theme.js
@@ -6,6 +6,7 @@ const tan = "#FECEA8"
 const lightRed = "#FF847C"
 const red = "#E84A5F"
 const offBlack = "#2A363B"
+const grey = "#747474"
 
 const theme = createTheme({
   palette: {
@@ -23,8 +24,41 @@ const theme = createTheme({
     },
   },
   typography: {
-    h1: {},
-    body1: {},
+    h1: {
+      fontSize: "4.5rem",
+      fontFamily: "Philosopher",
+      fontStyle: "italic",
+      fontWeight: 700,
+      color: green,
+    },
+    h2: {
+      fontFamily: "Montserrat",
+      fontSize: "3rem",
+      fontWeight: 500,
+    },
+    h3: {
+      fontFamily: "Montserrat",
+      fontSize: "2rem",
+      fontWeight: 300,
+      color: green,
+    },
+    h4: {
+      fontFamily: "Philosopher",
+      fontFamily: "italic",
+      fontWeight: 700,
+      fontSize: "3rem",
+    },
+    h5: {
+      fontFamily: "Philosopher",
+      fontSize: "2rem",
+      fontWeight: 700,
+      fontStyle: "italic",
+    },
+    body1: {
+      fontFamily: "Montserrat",
+      fontSize: "1.5rem",
+      color: grey,
+    },
   },
   overrides: {},
 })
