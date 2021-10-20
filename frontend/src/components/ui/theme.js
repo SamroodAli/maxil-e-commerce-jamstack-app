17f66d683c1f41e4eff3e7ad1dfd246912d66803
samroodAli <devsamrood@gmail.com>
2021-10-21 00:19:08 +0530

    Material-ui:Setup placeholders for palette , typography and overrides

--- frontend/src/components/ui/theme.js
+++ frontend/src/components/ui/theme.js
@@ -1,5 +1,9 @@
 import { createTheme } from "@mui/material/styles"
 
-const theme = createTheme()
+const theme = createTheme({
+  palette: {},
+  typography: {},
+  overrides: {},
+})
 
 export default theme
