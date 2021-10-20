5e9c73c4e0e32085103b97d53d400ce46b96a347
samroodAli <devsamrood@gmail.com>
2021-10-21 00:32:40 +0530

    Color:Use variables for the colors in theme

--- frontend/src/components/ui/theme.js
+++ frontend/src/components/ui/theme.js
@@ -1,18 +1,25 @@
 import { createTheme } from "@mui/material/styles"
 
+const green = "#99B898"
+const darkGreen = "#708670"
+const tan = "#FECEA8"
+const lightRed = "#FF847C"
+const red = "#E84A5F"
+const offBlack = "#2A363B"
+
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#99B898",
+      main: green,
     },
     secondary: {
-      main: "#708670",
+      main: darkGreen,
     },
     common: {
-      tan: "#FECEA8",
-      lightRed: "#FF847C",
-      red: "#E84A5F",
-      offBlack: "#2A363B",
+      tan,
+      lightRed,
+      red,
+      offBlack,
     },
   },
   typography: {
