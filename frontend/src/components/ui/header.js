eb2bfe495c749bd49daed47fa66227f6f7b06102
samroodAli <devsamrood@gmail.com>
2021-10-29 11:24:52 +0530

    Style: Style the header using makeStyles and useStyles

--- frontend/src/components/ui/header.js
+++ frontend/src/components/ui/header.js
@@ -6,19 +6,61 @@ import Tabs from "@material-ui/core/Tabs"
 import Tab from "@material-ui/core/Tab"
 import Toolbar from "@material-ui/core/Toolbar"
 
+import search from "../../images/search.svg"
+import cart from "../../images/cart.svg"
+import account from "../../images/account-header.svg"
+import IconButton from "@material-ui/core/IconButton"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles(theme => {
+  return {
+    coloredIndicator: {
+      backgroundColor: "#fff",
+    },
+    logoText: {
+      color: theme.palette.common.offBlack,
+    },
+    tabs: {
+      marginLeft: "auto",
+      marginRight: "auto",
+    },
+    // "@global": {
+    //   ".MuiTypography-h1": {
+    //     fontSize: "30rem",
+    //   },
+    // },
+  }
+})
+
 export default function Header() {
+  const classes = useStyles()
+
   return (
     <AppBar color="transparent" elevation={0}>
       <Toolbar>
         <Button>
-          <Typography variant="h1">VAR X</Typography>
+          <Typography variant="h1">
+            <span className={classes.logoText}>VAR</span> X
+          </Typography>
         </Button>
-        <Tabs>
+        <Tabs
+          value={0}
+          classes={{ indicator: classes.coloredIndicator, root: classes.tabs }}
+        >
           <Tab label="Hats" />
           <Tab label="Hoodies" />
           <Tab label="Shirts" />
           <Tab label="Contact Us" />
         </Tabs>
+        <IconButton>
+          <img src={search} alt="search" />
+        </IconButton>
+        <IconButton>
+          <img src={cart} alt="cart" />
+        </IconButton>
+        <IconButton>
+          <img src={account} alt="account" />
+        </IconButton>
       </Toolbar>
     </AppBar>
   )
