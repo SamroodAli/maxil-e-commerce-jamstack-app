3f50ea2c955b76667370ac5d1bbd94e659256884
samroodAli <devsamrood@gmail.com>
2021-10-31 11:09:03 +0530

    Graphql:Use graphql to query data from strapi for promotional products

--- frontend/src/components/home/promotionalProducts.js
+++ frontend/src/components/home/promotionalProducts.js
@@ -1,9 +1,28 @@
 import React from "react"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
-import { useStaticQuery } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 const PromotionalProducts = () => {
+  const data = useStaticQuery(graphql`
+    query PromotionalProductsQuery {
+      allStrapiProducts(filter: { promo: { eq: true } }) {
+        edges {
+          node {
+            name
+            strapiId
+            description
+            variants {
+              images {
+                url
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  console.log(data)
   return <div>Promo products</div>
 }
 
