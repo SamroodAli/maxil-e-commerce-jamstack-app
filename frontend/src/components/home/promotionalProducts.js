f1c86de200e0926f6f6887bd69260e7040d14937
samroodAli <devsamrood@gmail.com>
2021-10-31 11:45:17 +0530

    Promotional:Content add background

--- frontend/src/components/home/promotionalProducts.js
+++ frontend/src/components/home/promotionalProducts.js
@@ -1,9 +1,31 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
-import { useStaticQuery, graphql } from "gatsby"
+import Button from "@material-ui/core/Button"
+import IconButton from "@material-ui/core/IconButton"
+import Carousel from "react-spring-3d-carousel"
+import clsx from "clsx"
+import { makeStyles } from "@material-ui/core/styles"
+
+import promoAdornment from "../../images/promo-adornment.svg"
+import expore from "../../images/explore.svg"
+
+const useStyles = makeStyles(theme => ({
+  mainContainer: {
+    backgroundImage: `url(${promoAdornment})`,
+    backgroundPosition: "top",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "70rem",
+    padding: "30rem 10rem 10rem 10rem",
+  },
+}))
 
 const PromotionalProducts = () => {
+  const classes = useStyles()
+
   const data = useStaticQuery(graphql`
     query PromotionalProductsQuery {
       allStrapiProducts(filter: { promo: { eq: true } }) {
@@ -23,7 +45,17 @@ const PromotionalProducts = () => {
     }
   `)
   console.log(data)
-  return <div>Promo products</div>
+  return (
+    <Grid
+      container
+      justifyContent="space-between"
+      alignItems="center"
+      classes={{ root: classes.mainContainer }}
+    >
+      <Grid item>CArousel</Grid>
+      <Grid item>Description</Grid>
+    </Grid>
+  )
 }
 
 export default PromotionalProducts
