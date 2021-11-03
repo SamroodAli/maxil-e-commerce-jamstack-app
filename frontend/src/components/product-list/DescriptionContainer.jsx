76a368442086479f78d0c3ffa23ecd259958381e
samroodAli <devsamrood@gmail.com>
2021-11-04 00:13:52 +0530

    DescriptionContainer:Component render with props

---
+++ frontend/src/components/product-list/DescriptionContainer.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import Grid from "@material-ui/core/Grid"
+import Typography from "@material-ui/core/Typography"
+import makeStyles from "@material-ui/core/makeStyles"
+
+const useStyles = makeStyles(theme => ({
+  description: {
+    color: "#fff",
+  },
+}))
+
+const DescriptionContainer = ({ name, description }) => {
+  const classes = useStyles()
+  return (
+    <Grid item container>
+      <Grid item>
+        <Typography variant="h4" paragraph gutterBottom>
+          {name}
+        </Typography>
+        <Typography variant="body1" classes={{ root: classes.description }}>
+          {description}
+        </Typography>
+      </Grid>
+    </Grid>
+  )
+}
+
+export default DescriptionContainer
--- frontend/src/templates/ProductList.jsx
+++ frontend/src/templates/ProductList.jsx
@@ -3,12 +3,14 @@ import Grid from "@material-ui/core/Grid"
 
 import Layout from "../components/ui/Layout"
 import DynamicToolbar from "../components/product-list/DynamicToolbar"
+import DescriptionContainer from "../components/product-list/DescriptionContainer"
 
-const ProductList = ({ pageContext }) => {
+const ProductList = ({ pageContext: { filterOptions, name, description } }) => {
   return (
     <Layout>
       <Grid container direction="column" alignItems="center">
-        <DynamicToolbar filterOptions={pageContext.filterOptions} />
+        <DynamicToolbar filterOptions={filterOptions} />
+        <DescriptionContainer name={name} description={description} />
       </Grid>
     </Layout>
   )
