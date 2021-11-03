b67e94327fce3b19dde0457ab0af733c85fb1779
samroodAli <devsamrood@gmail.com>
2021-11-03 18:50:55 +0530

    PageQuery:Using gatsby-node api createPages

--- frontend/gatsby-node.js
+++ frontend/gatsby-node.js
@@ -5,3 +5,50 @@
  */
 
 // You can delete this file if you're not using it
+
+exports.createPages = async ({ graphql, action }) => {
+  const { createPage } = action
+
+  const results = await graphql(
+    `
+      query pageQuery {
+        products: allStrapiProducts {
+          edges {
+            node {
+              name
+              strapiId
+            }
+          }
+        }
+        categories: allStrapiCategories {
+          edges {
+            node {
+              strapiId
+              name
+              description
+            }
+          }
+        }
+      }
+    `
+  )
+
+  const products = results.data.products.edges
+  const categories = results.data.categories.edges
+
+  // products.forEach(product => {
+  //   createPage({
+  //     path:`/${p}`,
+  //     component:,
+  //     context:{}
+  //   })
+  // })
+
+  // categories.forEach(category => {
+  //   createPage({
+  //     path:`/${category.node.name.toLowerCase()}`,
+  //     component:,
+  //     context:{}
+  //   })
+  // })
+}
