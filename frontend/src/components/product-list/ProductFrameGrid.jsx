2c2eff83b0a7958e23af434cb9a2c7ae2f58ae62
samroodAli <devsamrood@gmail.com>
2021-11-04 19:18:46 +0530

    Products:Display images from strapi in the grid

--- frontend/src/components/product-list/ProductFrameGrid.jsx
+++ frontend/src/components/product-list/ProductFrameGrid.jsx
@@ -13,15 +13,28 @@ const useStyles = makeStyles(theme => ({
     backgroundRepeat: "no-repeat",
     height: "25rem",
     width: "25rem",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  product: {
+    height: "20rem",
+    width: "20rem",
   },
 }))
 
-const ProductFrameGrid = ({ products }) => {
+const ProductFrameGrid = ({ product, variant }) => {
   const classes = useStyles()
   return (
     <Grid item>
       <Grid container direction="column">
-        <Grid item classes={{ root: classes.frame }}></Grid>
+        <Grid item classes={{ root: classes.frame }}>
+          <img
+            className={classes.product}
+            src={process.env.GATSBY_STRAPI_API_URL + variant.images[0].url}
+            alt={product.node.name}
+          />
+        </Grid>
       </Grid>
     </Grid>
   )
