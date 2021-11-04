00df453446b7c19758818714366bff9a3c016b20
samroodAli <devsamrood@gmail.com>
2021-11-04 21:24:21 +0530

    State:Select size in quick view using state

--- frontend/src/components/product-list/Sizes.jsx
+++ frontend/src/components/product-list/Sizes.jsx
@@ -3,6 +3,7 @@ import Grid from "@material-ui/core/Grid"
 import Typography from "@material-ui/core/Typography"
 import { makeStyles } from "@material-ui/core/styles"
 import Button from "@material-ui/core/Button"
+import clsx from "clsx"
 
 const useStyles = makeStyles(theme => ({
   size: {
@@ -14,6 +15,12 @@ const useStyles = makeStyles(theme => ({
     height: "3rem",
     width: "3rem",
     minWidth: 0,
+  },
+  selected: {
+    backgroundColor: theme.palette.secondary.main,
+    "&:hover": {
+      backgroundColor: theme.palette.secondary.main,
+    },
   },
 }))
 
@@ -32,7 +39,11 @@ const Sizes = ({ sizes, selectedSize, setSelectedSize }) => {
       {actualSizes.map(size => (
         <Grid item key={size}>
           <Button
-            classes={{ root: classes.button }}
+            classes={{
+              root: clsx(classes.button, {
+                [classes.selected]: selectedSize === size,
+              }),
+            }}
             onClick={() => setSelectedSize(size)}
           >
             <Typography variant="h3" classes={{ root: classes.size }}>
