1205c977c402cb89c0423a7d5cf51e4aeabfb35e
samroodAli <devsamrood@gmail.com>
2021-11-04 19:12:34 +0530

    Product:List grid view with calculating margin right with css calc

--- frontend/src/components/product-list/ListOfProducts.jsx
+++ frontend/src/components/product-list/ListOfProducts.jsx
@@ -5,12 +5,23 @@ import { makeStyles } from "@material-ui/core/styles"
 
 import ProductFrameGrid from "./ProductFrameGrid"
 
-const useStyles = makeStyles(theme => ({}))
+const useStyles = makeStyles(theme => ({
+  productContainer: {
+    width: "95%",
+    "& > *": {
+      marginRight: "calc((100% - (25rem * 4)) / 3)",
+      marginBottom: "5rem",
+    },
+    "& > :nth-child(4n)": {
+      marginRight: 0,
+    },
+  },
+}))
 
 const ListOfProducts = ({ products }) => {
   const classes = useStyles()
   return (
-    <Grid item container>
+    <Grid item container classes={{ root: classes.productContainer }}>
       {products.map(product =>
         product.node.variants.map(variant => (
           <ProductFrameGrid
